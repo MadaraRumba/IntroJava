0896d39f32468ef4845f1dbe8d8a934619ead9bd
MadaraRumba <madara.rumba.rumba@gmail.com>
2022-02-26 12:29:59 +0200

    hi

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float number; //variable declaration
+        number = 4; // variable initialization
+
+        float value = 6; // variable declaration and initialization in one line
+//        byte byteValue = 1;
+
+//        char charValue = '9';
+//        String text = "I am going to school";
+
+//        long digits = 5L;
+
+       // float decimalFloat = 3.683f; // has a max of 7 decimal places
+        //double decimalDouble = 5.232323; // has a max of 15 decimal places
+
+        // Arithmetic Operators include:
+        // + - Addition
+        System.out.println("Addition of number and value is:" + (number + value));
+        // - - Subtraction
+        System.out.println("Subtraction of number and value is:" + (number - value));
+        // / - Division
+        System.out.println("Division of number and value is:" + (number / value));
+        // * - Multiplication
+        System.out.println("Multiplication of number and value is:" + (number * value));
+        // % - Modulus (It divides the values and returns the remainder) e. g. 2 % 2 -> 0
+        System.out.println("Modular of number and value is:" + (number % value));
+
+        //short cut to comment a line of code: CTRL + /
+
+        // Our first Java program
+//        System.out.println("Hello World!");
+
+    }
+}
